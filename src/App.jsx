2684f5af6207a78b948d5b7b9c738c3d21243520
Dreiiur <131451794+Dreiiur@users.noreply.github.com>
2025-01-11 22:18:46 +0800

    refactor :  none

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const fadeIn = {
+  const fadeIn =  {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.2 } },
   };
