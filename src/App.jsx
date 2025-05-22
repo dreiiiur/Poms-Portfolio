4e5ee270d473adbed7232cc53049bd1faf1fce8e
Dreiiur <131451794+Dreiiur@users.noreply.github.com>
2025-05-22 21:51:48 +0800

    refactor: dirt

--- src/App.jsx
+++ src/App.jsx
@@ -684,9 +684,6 @@ function App() {
       id="testimonials"
     >
       {/* Minimal Background Effects */}
-      <div className="absolute top-0 left-0 w-72 h-72 bg-gray-300/40 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2 z-0"></div>
-      <div className="absolute bottom-0 right-0 w-96 h-96 bg-gray-500/50 rounded-full blur-2xl translate-x-1/3 translate-y-1/3 z-0"></div>
-       <div className="absolute top-0 left-0 w-72 h-72 bg-gray-300/40 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2 z-0"></div>
       <div className="absolute bottom-0 right-0 w-96 h-96 bg-gray-500/50 rounded-full blur-2xl translate-x-1/3 translate-y-1/3 z-0"></div>
 
       {/* Main Content */}
